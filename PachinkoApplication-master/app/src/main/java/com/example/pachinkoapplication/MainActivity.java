8a801e347ac53c596dffb4b25f25bbeca423f428
ten3sei <tensho6686@gmail.com>
2020-11-08 15:51:41 +0900

    Initial commitえｗ

---
+++ PachinkoApplication-master/app/src/main/java/com/example/pachinkoapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pachinkoapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+//私はない投与
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
