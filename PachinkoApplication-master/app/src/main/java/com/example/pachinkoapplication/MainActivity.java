18b635dcb4df1ae523a1d239968d484ff3ec0d17
gyogyoguma <gyogyoguma@gmail.com>
2020-11-08 16:21:14 +0900

    aaa

--- PachinkoApplication-master/app/src/main/java/com/example/pachinkoapplication/MainActivity.java
+++ PachinkoApplication-master/app/src/main/java/com/example/pachinkoapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-//私はない投与
+//私はない投与aaaaaaaaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
